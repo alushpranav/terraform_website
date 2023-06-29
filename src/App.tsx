28eb774e13633ceba600be984fbd21b6fc30bb15
alushpranav <alushpranav@gmail.com>
2023-06-29 09:48:16 +0530

    basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Help from "./pages/help";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/help",
+    element: <Help />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>MAINSCREEN</h1>
+      <ul>
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/help">HELP</a>
+        </li>
+        <li>
+          <a href="/profile">PROFILE</a>
+        </li>
+      </ul>
+      <RouterProvider router={router} />
     </>
   );
 }
